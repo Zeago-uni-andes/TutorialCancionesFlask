553837287f52c9397c20c3831c06bd386c108bd2
Juan David Zea Gonzalez <70subtecpro2@FOPSUBTECPROF2.local>
2026-08-24 16:01:19 -0500

    feat: jwt canciones

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,4 +4,10 @@ def create_app(config_name):
     app=Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_canciones'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] =  False
+
+    app.config['JWT_SECRET_KEY'] = 'FRASE-SECRETA'
+
+    app.config['PROPAGATE_EXCEPTIONS'] = True
+
+
     return app
--- flaskr/app.py
+++ flaskr/app.py
@@ -2,7 +2,8 @@ from flaskr import create_app
 from .modelos import db, Cancion, Album, Usuario, Medio
 from .modelos import AlbumSchema,UsuarioSchema,CancionSchema
 from flask_restful import Api
-from .vistas import VistaCanciones,VistaCancion
+from .vistas import VistaCanciones, VistaCancion, VistaSignIn, VistaLogIn, VistaAlbum, VistaAlbumsUsuario, VistaCancionesAlbum
+from flask_jwt_extended import JWTManager
 
 app= create_app('default')
 app_context = app.app_context()
@@ -12,9 +13,15 @@ db.init_app(app)
 db.create_all()
 
 api = Api(app)
-api.add_resource(VistaCanciones,'/canciones')
-api.add_resource(VistaCancion,'/cancion/<int:id_cancion>')
+api.add_resource(VistaCanciones, '/canciones')
+api.add_resource(VistaCancion, '/cancion/<int:id_cancion>')
+api.add_resource(VistaSignIn, '/signin')
+api.add_resource(VistaLogIn, '/login')
+api.add_resource(VistaAlbumsUsuario, '/usuario/<int:id_usuario>/albumes')
+api.add_resource(VistaAlbum, '/album/<int:id_album>')
+api.add_resource(VistaCancionesAlbum, '/album/<int:id_album>/canciones')
 
+jwt = JWTManager(app)
 
 # # 3. Bloque de prueba
 # with app.app_context():
